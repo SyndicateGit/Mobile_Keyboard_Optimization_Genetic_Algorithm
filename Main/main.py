c97fcf162e5ec5f9e8dab0e99ca5dffc85cb6d0e
Raymond Zeng <shilei.zeng98@outlook.com>
2024-03-22 18:45:46 -0400

    Changed main import

--- Main/main.py
+++ Main/main.py
@@ -1,7 +1,7 @@
 from KeyboardObject import Keyboard
 import KeySwap
-from Mobile_Keyboard_Optimization_Genetic_Algorithm.GUI import GUI
-from Mobile_Keyboard_Optimization_Genetic_Algorithm.Evaluation_Pipeline import Evaluate
+from GUI import GUI
+from Evaluation_Pipeline import Evaluate
 
 # Main is where we do the scripting for the pipeline and generate the
 # list of keyboards representing the top performing keyboard of a generation
