92d467bc7a15d41c4010841dcb59440ace998d67
Salman Youssef <salman@Salmans-MacBook-Pro-2.local>
2024-03-22 19:05:02 -0400

    Evaluate reddit dataset

--- Evaluation_Pipeline/Evaluate.py
+++ Evaluation_Pipeline/Evaluate.py
@@ -10,37 +10,37 @@ import csv
 
 text_data = "marryhadalittlelamb.123"
 
-# max_rows = 10000  # The file is very big, so we should choose a max_rows to be read from the data
+max_rows = 100  # The file is very big, so we should choose a max_rows to be read from the data
 
-# # Openning the Data file and only storing 
-# # relevant information in the text_data variable.
-# # TODO: csv_reader had unicode decoder error. 
-# with open("the-reddit-dataset-dataset-comments.csv", "r") as file:
-#     # Create a CSV reader object
-#     csv_reader = csv.DictReader(file)
+# Openning the Data file and only storing 
+# relevant information in the text_data variable.
+# TODO: csv_reader had unicode decoder error. 
+with open("the-reddit-dataset-dataset-comments.csv", "r") as file:
+    # Create a CSV reader object
+    csv_reader = csv.DictReader(file)
     
-#     for i, row in enumerate(csv_reader):
-#         if "[deleted]" in row["body"]:
-#             continue
-#         elif "[removed]" in row["body"]:
-#             continue
-#         elif "I am a bot" in row["body"]:
-#             continue
-#         # Removing any posts with links because they include many
-#         # special characters that would usually not be types by the user.
-#         elif "https://" in row["body"]:
-#             continue
-#         elif "&amp" in row["body"]:
-#             continue
-#         elif "http://" in row["body"]:
-#             continue
-#         elif "[website]" in row["body"]:
-#             continue
+    for i, row in enumerate(csv_reader):
+        if "[deleted]" in row["body"]:
+            continue
+        elif "[removed]" in row["body"]:
+            continue
+        elif "I am a bot" in row["body"]:
+            continue
+        # Removing any posts with links because they include many
+        # special characters that would usually not be types by the user.
+        elif "https://" in row["body"]:
+            continue
+        elif "&amp" in row["body"]:
+            continue
+        elif "http://" in row["body"]:
+            continue
+        elif "[website]" in row["body"]:
+            continue
         
-#         text_data += row["body"]
+        text_data += row["body"]
         
-#         if i + 1 >= max_rows:
-#             break 
+        if i + 1 >= max_rows:
+            break 
 
 # print(text_data)
 
@@ -128,3 +128,5 @@ test_texts = [
 for text in test_texts:
     print("\nEvaluating text:", text)
     evaluate_keyboard(test_keyboard, text)
+
+evaluate_keyboard(test_keyboard, text_data)
